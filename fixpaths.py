1f9eeb267a7e5c501690e8d509cb3ab312952e98
Jaesan Ryfle-Turi <88599935+MatuaDoc@users.noreply.github.com>
2024-09-11 22:39:34 +1200

    FIX: no longer append new contents, overwrite only

--- fixpaths.py
+++ fixpaths.py
@@ -6,10 +6,12 @@ def replace_text_in_file(target_text, file_path):
     print(folder_name)
 
     # Read the file contents
-    with open(file_path, 'r+') as file:
+    with open(file_path, 'r') as file:
         file_contents = file.read()
-        updated_contents = file_contents.replace(target_text, folder_name)
-        print(updated_contents)
+    
+    updated_contents = file_contents.replace(target_text, folder_name)
+    
+    with open(file_path, 'w') as file:
         file.write(updated_contents)
 
     print(f"Replaced '{target_text}' with '{folder_name}' in {file_path}")
