67cea5448fa9fdf63387a23d5e3b56680f46ab07
Jaesan Ryfle-Turi <88599935+MatuaDoc@users.noreply.github.com>
2024-09-23 09:17:58 +1200

    Create template with OCTools pre-imported

--- Program.swift
+++
@@ -1,6 +0,0 @@
-let name = "Anahera"
-print(name)
-
-var score = "2"
-score = "5"
-print(score)
---
+++ Template.swift
@@ -0,0 +1,7 @@
+// Template.swift
+// Created by NAME.
+// Created on DATE.
+
+import OCTools // @OnslowCollege
+
+// Delete this comment, then write your code here.
