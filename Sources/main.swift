62be051374148c94bdee5ec821bf8fb1dc24a3da
Jaesan Ryfle-Turi <88599935+MatuaDoc@users.noreply.github.com>
2024-10-13 07:40:32 +0000

    Oct 13, 2024, 7:40 AM

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,9 +2,11 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
+import OCFoundation
 
 // Instructions:
 // 1. Create and new branch and switch to it.
 // 2. Remove these comments and the code below.
 // 3. Write your code.
-print("Hello, world!")
+let name = input("Enter your name: ")
+print("Hello, \(name)!")
