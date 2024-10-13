a65ab42b95bacbac1b9c50b32eef7ee2356e46d3
Jaesan Ryfle-Turi <88599935+MatuaDoc@users.noreply.github.com>
2024-10-13 07:34:19 +0000

    Oct 13, 2024, 7:34 AM

--- Package.swift
+++ Package.swift
@@ -8,13 +8,16 @@ let package = Package(
     dependencies: [
         // ...
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.54.0"),
+        .package(url: "https://github.com/OnslowCollege/OCFoundation", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "Program",
-            dependencies: []
-            )
+            dependencies: [
+                "OCFoundation"
+            ]
+        )
     ]
 )
